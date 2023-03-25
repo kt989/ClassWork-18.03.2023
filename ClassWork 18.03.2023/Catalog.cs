0456cfae4cc2a7ee647e84063e6fc7784545effb
Gladk_vs07@top-academy.ru <Gladk_vs07@top-academy.ru>
2023-03-25 08:59:53 +0300

    Add project files.

---
+++ ClassWork 18.03.2023/Catalog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_18._03._2023
+{
+    public class Catalog
+    {
+        public string Parent { get; private set; }
+        public string SelectDirectoryPath { get; private set; }
+        public List<string> Directories = new();
+        /// <summary>
+        /// Модель, хранящая в себе свойства каталога
+        /// </summary>
+        public Catalog() => GetRootCatalog();
+        //public Catalog() => Test();
+        public void GetRootCatalog()
+        {
+            Directories = DriveInfo.GetDrives().Select(item=>item.Name).ToList();
+            Parent = "root";
+            SelectDirectoryPath = Parent;
+            /* Directories = new List <string>();
+             var dir = DriveInfo.GetDrives();
+             // Directories = DriveInfo.GetDrives(); //получаем списоок дисков компьютера
+             foreach (var item in dir)
+             {
+                 Directories.Add(item.Name);
+             }*/
+        }
+
+        public void Test()
+        {
+            var dir= DriveInfo.GetDrives().Select(item => item.Name).ToList();
+            var dirinfo = Directory.GetFileSystemEntries(dir.First()).ToList();
+            Directories = dirinfo;
+        }
+    }
+}
---
+++ ClassWork 18.03.2023/Program.cs
@@ -0,0 +1,51 @@
+﻿using ClassWork_18._03._2023;
+
+var obj = new Catalog();
+Main();
+void Main()
+{
+    Console.CursorVisible = false;
+    DisplayMenu();
+    int index = 2;
+    while (true)
+    { 
+    switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.UpArrow:
+                {
+                    if (index > 2)
+                        SelectMenuItem(--index, index + 1);
+                    break;
+                }
+            case ConsoleKey.DownArrow:
+                {
+                    if (index < obj.Directories.Count+1)
+                        SelectMenuItem(++index, index - 1);
+                    break;
+                }
+        }
+    }
+}
+
+void DisplayMenu()
+{
+    Console.Clear();
+    Console.WriteLine($"Текущий каталог: {obj.SelectDirectoryPath} "+"\n"+"----------------------------------------------------------------------------------------");
+    foreach (var item in obj.Directories)
+    {
+        Console.WriteLine(item==obj.Directories.First()
+            ?" -> "+item
+            :"    "+item);
+    }
+}
+
+void SelectMenuItem(int end, int start)
+{
+    for (int i=start; ;i=end)
+    {
+        Console.SetCursorPosition(0, i);
+        Console.WriteLine(i==start
+            ?"    ":" -> " + obj.Directories[i-2]);
+        if (i == end) break;
+    }
+}
