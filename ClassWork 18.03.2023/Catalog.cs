8ca08135b6a0007bddca865fa6cf94e3ada7079a
Gladk_vs07@top-academy.ru <Gladk_vs07@top-academy.ru>
2023-03-25 12:39:04 +0300

    All gotovo

--- ClassWork 18.03.2023/Catalog.cs
+++ ClassWork 18.03.2023/Catalog.cs
@@ -11,30 +11,51 @@ namespace ClassWork_18._03._2023
         public string Parent { get; private set; }
         public string SelectDirectoryPath { get; private set; }
         public List<string> Directories = new();
+        public bool IsCatalog { get; private set; } = true;
         /// <summary>
         /// Модель, хранящая в себе свойства каталога
         /// </summary>
-        public Catalog() => GetRootCatalog();
-        //public Catalog() => Test();
+        public Catalog()=> GetRootCatalog();
+    
         public void GetRootCatalog()
         {
-            Directories = DriveInfo.GetDrives().Select(item=>item.Name).ToList();
+            Directories = DriveInfo.GetDrives().Select(item=>item.Name).ToList(); // получаем список дисков с их свойствами и записываем только имена
             Parent = "root";
             SelectDirectoryPath = Parent;
-            /* Directories = new List <string>();
-             var dir = DriveInfo.GetDrives();
-             // Directories = DriveInfo.GetDrives(); //получаем списоок дисков компьютера
-             foreach (var item in dir)
-             {
-                 Directories.Add(item.Name);
-             }*/
         }
 
-        public void Test()
+        public void UpdateCatalog(string directory)
+        {
+            var item = SelectDirectoryPath is not "root" && directory!=Parent
+                ? SelectDirectoryPath + @"\" + directory
+                : directory;
+             var dir_info = new DirectoryInfo(item);
+            try 
+            {
+                Directories = Directory.GetFileSystemEntries(item).Select(i=>Path.GetFileName(i)).ToList();
+                SelectDirectoryPath = item;
+                Parent = dir_info.Parent is not null
+                    ? dir_info.Parent.ToString()
+                    : "root";
+                IsCatalog= true;
+            }
+            catch(Exception ex) 
+            { 
+                Directories.Clear();
+                Directories.Add(ex.Message);
+                Parent = SelectDirectoryPath;
+                IsCatalog= false;
+            }
+
+        }
+
+       /* public void GetDownCatalog() // метод для тестирования
         {
             var dir= DriveInfo.GetDrives().Select(item => item.Name).ToList();
             var dirinfo = Directory.GetFileSystemEntries(dir.First()).ToList();
             Directories = dirinfo;
-        }
+            Parent = "DownCatalog";
+            SelectDirectoryPath = Parent;
+        }*/
     }
 }
--- ClassWork 18.03.2023/Program.cs
+++ ClassWork 18.03.2023/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ClassWork_18._03._2023;
+using System;
+using System.Diagnostics.Metrics;
 
+//var obj = new Catalog(0);
 var obj = new Catalog();
 Main();
 void Main()
@@ -8,8 +11,8 @@ void Main()
     DisplayMenu();
     int index = 2;
     while (true)
-    { 
-    switch (Console.ReadKey().Key)
+    {
+        switch (Console.ReadKey().Key)
         {
             case ConsoleKey.UpArrow:
                 {
@@ -19,33 +22,56 @@ void Main()
                 }
             case ConsoleKey.DownArrow:
                 {
-                    if (index < obj.Directories.Count+1)
+                    if (index < obj.Directories.Count + 1)
                         SelectMenuItem(++index, index - 1);
+                    break;
+                }
+            case ConsoleKey.LeftArrow: 
+                {
+                    UpdateMenu(obj.Parent, ref index);
+                    break;
+                }
+            case ConsoleKey.RightArrow:
+                {
+                    if (obj.Directories.Count > 0)
+                        UpdateMenu(obj.Directories[index - 2], ref index);
                     break;
                 }
         }
     }
-}
 
-void DisplayMenu()
-{
-    Console.Clear();
-    Console.WriteLine($"Текущий каталог: {obj.SelectDirectoryPath} "+"\n"+"----------------------------------------------------------------------------------------");
-    foreach (var item in obj.Directories)
+    void DisplayMenu()
     {
-        Console.WriteLine(item==obj.Directories.First()
-            ?" -> "+item
-            :"    "+item);
+        Console.Clear();
+        Console.WriteLine($"Текущий каталог: {obj.SelectDirectoryPath} " + "\n" + "----------------------------------------------------------------------------------------");
+        foreach (var item in obj.Directories)
+        {
+            Console.WriteLine(item == obj.Directories.First() && obj.IsCatalog
+                ? " -> " + item
+                : "    " + item);
+        }
+    }
+
+
+    void SelectMenuItem(int end, int start)
+    {
+        for (int i = start; ; i = end)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.WriteLine(i == start
+                ? "    " : " -> " + obj.Directories[i - 2]);
+            if (i == end) break;
+        }
+    }
+
+    void UpdateMenu(string item, ref int index)
+    {
+        if (item is "root")
+            obj.GetRootCatalog();
+        else
+            obj.UpdateCatalog(item);
+        DisplayMenu();
+        index = 2;
     }
 }
 
-void SelectMenuItem(int end, int start)
-{
-    for (int i=start; ;i=end)
-    {
-        Console.SetCursorPosition(0, i);
-        Console.WriteLine(i==start
-            ?"    ":" -> " + obj.Directories[i-2]);
-        if (i == end) break;
-    }
-}
